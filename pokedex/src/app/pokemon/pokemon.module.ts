638a87a1eced59eac663804259952cb7a7793d7e
Anastasia Yasyutina <a.yasutina@gmail.com>
2020-03-22 18:02:49 +0300

    generated necessary components

--- pokedex/src/app/pokemon/pokemon.module.ts
+++ pokedex/src/app/pokemon/pokemon.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { PokemonsPageComponent } from './pokemons-page/pokemons-page.component';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, PokemonsPageComponent, PokemonCardComponent],
   imports: [
     CommonModule
   ]
