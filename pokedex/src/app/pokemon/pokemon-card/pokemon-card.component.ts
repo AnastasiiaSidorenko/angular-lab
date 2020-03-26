3d06a48625050008c3f3999cff940415a52bdf09
Anastasiia Sidorenko <anastasiia_sidorenko@epam.com>
2020-03-26 10:16:25 +0300

    added setFreeButton

--- pokedex/src/app/pokemon/pokemon-card/pokemon-card.component.ts
+++ pokedex/src/app/pokemon/pokemon-card/pokemon-card.component.ts
@@ -6,11 +6,17 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./pokemon-card.component.scss']
 })
 export class PokemonCardComponent implements OnInit {
+  setFreeButton = false;
   @Input() pokemon;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  switchButton(name) {
+    this.setFreeButton = !this.setFreeButton;
+    console.log(`Pokemon ${name} ${this.setFreeButton ? 'is caught' : 'is free'}`);
   }
 
 }
