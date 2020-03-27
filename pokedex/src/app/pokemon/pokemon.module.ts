fd9ddd338ad93bbba372b12f652b559cc9522a80
Anastasiia Sidorenko <anastasiia_sidorenko@epam.com>
2020-03-27 10:10:47 +0300

    added pokemon-list component

--- pokedex/src/app/pokemon/pokemon.module.ts
+++ pokedex/src/app/pokemon/pokemon.module.ts
@@ -4,9 +4,10 @@ import { HeaderComponent } from './header/header.component';
 import { PokemonsPageComponent } from './pokemons-page/pokemons-page.component';
 import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 import { RedBoldDirective } from './directives/card-border.directive';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
 @NgModule({
-  declarations: [HeaderComponent, PokemonsPageComponent, PokemonCardComponent, RedBoldDirective],
+  declarations: [HeaderComponent, PokemonsPageComponent, PokemonCardComponent, RedBoldDirective, PokemonListComponent],
   imports: [CommonModule],
   exports: [PokemonsPageComponent, HeaderComponent, PokemonCardComponent]
 })
