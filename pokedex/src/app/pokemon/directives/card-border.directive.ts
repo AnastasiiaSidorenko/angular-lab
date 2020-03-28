1c6d6c745f5571ba5e771fef20819db9973dea79
Anastasiia Sidorenko <anastasiia_sidorenko@epam.com>
2020-03-28 19:35:09 +0300

    fixed ts lint feedback

--- pokedex/src/app/pokemon/directives/card-border.directive.ts
+++ pokedex/src/app/pokemon/directives/card-border.directive.ts
@@ -1,10 +1,10 @@
 import { Directive, ElementRef, Renderer2, AfterContentInit, Input } from '@angular/core';
 
 @Directive({
-    selector: '[red-bold]'
+    selector: '[appRedBold]'
 })
 export class RedBoldDirective implements AfterContentInit {
-    @Input('red-bold') damage: number;
+    @Input('appRedBold') damage: number;
 
     constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
