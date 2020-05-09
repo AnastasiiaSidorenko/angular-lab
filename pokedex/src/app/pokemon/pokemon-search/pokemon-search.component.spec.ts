3580d8813a3a704cdd11d976c56c604a32dab869
Anastasiia Sidorenko <anastasiia_sidorenko@epam.com>
2020-05-09 14:32:06 +0300

    test for pokemon-search component

--- pokedex/src/app/pokemon/pokemon-search/pokemon-search.component.spec.ts
+++ pokedex/src/app/pokemon/pokemon-search/pokemon-search.component.spec.ts
@@ -1,16 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PokemonSearchComponent } from './pokemon-search.component';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
-describe('PokemonSearchComponent', () => {
+fdescribe('PokemonSearchComponent', () => {
   let component: PokemonSearchComponent;
   let fixture: ComponentFixture<PokemonSearchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PokemonSearchComponent ]
+      declarations: [PokemonSearchComponent],
+      imports: [FormsModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +24,28 @@ describe('PokemonSearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit on click', () => {
+    spyOn(component.changed, 'emit');
+
+    const button = fixture.nativeElement.querySelector('.btn');
+    button.click();
+
+    expect(component.changed.emit).toHaveBeenCalled();
+  });
+
+  it('should test the emitter on click with argument', () => {
+    spyOn(component.changed, 'emit');
+
+    const input = fixture.debugElement.query(By.css('input'));
+    const el = input.nativeElement;
+    el.value = 'Pikachu';
+    el.dispatchEvent(new Event('input'));
+
+    const button = fixture.nativeElement.querySelector('.btn');
+    button.click();
+
+    expect(component.changed.emit).toHaveBeenCalledWith('Pikachu');
   });
 });
